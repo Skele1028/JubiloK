6942ca2a7c1ee4666598e46cb42a8858fa6e9771
Daniela Diaz Bejarano <60675531+DanielaDiazBej@users.noreply.github.com>
2021-11-06 12:24:49 -0500

    prueba

---
+++ js/app.js
@@ -0,0 +1,116 @@
+const webcamElement = document.getElementById('webcam');
+
+const canvasElement = document.getElementById('canvas');
+
+const webcam = new Webcam(webcamElement, 'user', canvasElement);
+
+
+$("#button-start").click(function() {
+    $('.md-modal').addClass('md-show');
+    webcam.start()
+        .then(result =>{
+           cameraStarted();
+           console.log("webcam started");
+        })
+        .catch(err => {
+            displayError();
+        });
+  });
+
+
+$('#cameraswitch').click(function() {
+    webcam.flip();
+    webcam.start();  
+});
+
+$('#closeError').click(function() {
+    $("#webcam-switch").prop('checked', false).change();
+});
+
+function displayError(err = ''){
+    if(err!=''){
+        $("#errorMsg").html(err);
+    }
+    $("#errorMsg").removeClass("d-none");
+}
+
+function cameraStarted(){
+    $("#errorMsg").addClass("d-none");
+    $('.flash').hide();
+    $("#webcam-caption").html("on");
+    $("#webcam-control").removeClass("webcam-off");
+    $("#webcam-control").addClass("webcam-on");
+    $("#webcam-control").addClass("d-none");
+    $(".webcam-container").removeClass("d-none");
+    $("#button-start").addClass("d-none");
+    if( webcam.webcamList.length > 1){
+        $("#cameraswitch").removeClass('d-none');
+    }
+    $("#wpfront-scroll-top-container").addClass("d-none");
+    window.scrollTo(0, 0); 
+    $('body').css('overflow-y','hidden');
+}
+
+function cameraStopped(){
+    $("#errorMsg").addClass("d-none");
+    $("#wpfront-scroll-top-container").removeClass("d-none");
+    $("#webcam-control").removeClass("webcam-on");
+    $("#webcam-control").addClass("webcam-off");
+    $("#cameraswitch").addClass('d-none');
+    $(".webcam-container").addClass("d-none");
+    $("#webcam-caption").html("Click to Start Camera");
+    $('.md-modal').removeClass('md-show');
+}
+
+
+$("#takePhoto").click(function () {
+    beforeTakePhoto();
+    let picture = webcam.snap();
+    document.querySelector('#download-photo').href = picture;
+    afterTakePhoto();
+});
+
+function beforeTakePhoto(){
+    $('.flash')
+        .show() 
+        .animate({opacity: 0.3}, 500) 
+        .fadeOut(500)
+        .css({'opacity': 0.7});
+    window.scrollTo(0, 0); 
+    $('#webcam-control').addClass('d-none');
+    $('#cameraControls').addClass('d-none');
+}
+
+function afterTakePhoto(){
+    webcam.stop();
+    $('#canvas').removeClass('d-none');
+    $('#takePhoto').addClass('d-none');
+    $('#exit-app').removeClass('d-none');
+    $('#download-photo').removeClass('d-none');
+    $('#resume-camera').removeClass('d-none');
+    $('#cameraControls').removeClass('d-none');
+    $("#cameraswitch").addClass('d-none');
+}
+
+function removeCapture(){
+    $('#canvas').addClass('d-none');
+    $('#webcam-control').removeClass('d-none');
+    $('#cameraControls').removeClass('d-none');
+    $('#takePhoto').removeClass('d-none');
+    $('#exit-app').addClass('d-none');
+    $('#download-photo').addClass('d-none');
+    $('#resume-camera').addClass('d-none');
+}
+
+$("#resume-camera").click(function () {
+    webcam.stream()
+        .then(facingMode =>{
+            removeCapture();
+        });
+});
+
+$("#exit-app").click(function () {
+    removeCapture();
+    $("#webcam-switch").prop("checked", false).change();
+});
+
