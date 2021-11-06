a43e75fe05b1b59ba59eba444949d8466f1ec09e
Daniela Diaz Bejarano <60675531+DanielaDiazBej@users.noreply.github.com>
2021-11-06 14:03:59 -0500

    integracion

--- js/webcam-easy.js
+++ js/webcam-easy.js
@@ -165,8 +165,28 @@ class Webcam {
 
   snap() {
     if (this._canvasElement != null) {
-  
-      
+
+      const $boton = document.querySelector("#btnCapturar"), // El botón que desencadena
+        $objetivo = document.body; // A qué le tomamos la fotocanvas
+      // Nota: no necesitamos contenedor, pues vamos a descargarla
+
+      // Agregar el listener al botón
+      $boton.addEventListener("click", () => {
+        html2canvas($objetivo) // Llamar a html2canvas y pasarle el elemento
+
+          .then(canvas => {
+            alert("se tomo captura de pantalla");
+            // Cuando se resuelva la promesa traerá el canvas
+            // Crear un elemento <a>
+            /*  enlace.crossOrigin = "anonymous";  // This enables CORS */
+            let enlace = document.createElement('a');
+            enlace.download = "Imagen.png";
+            // Convertir la imagen a Base64
+            enlace.href = canvas.toDataURL();
+            // Hacer click en él
+            enlace.click();
+          });
+      });
       /* 
         this._canvasElement.height = this._webcamElement.scrollHeight;
         this._canvasElement.width = this._webcamElement.scrollWidth;
